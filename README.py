490ddca046f31792a390753d7014767b4b156592
tkimx7 <55491619+tkimx7@users.noreply.github.com>
2022-06-01 01:40:45 -0400

    Update README.py

--- README.py
+++ README.py
@@ -6,7 +6,7 @@
 ### --- Dow Jones Industrial Average, at an ETF or a structured products division of a major bank or asset 
 ### --- manager would look like. Needless to say it's overly simplified, but the essentials are there:
 ### ---    (1) Weighing all components of a basket, i.e. a basket of stocks, by their price levels
-### ---    (2) Standardizing the index to begin at 100 by factoring returns since inception. This step is because 
+### ---    (2) Standardizing the index to begin at 100 by factoring returns since inception. This step is to reflect that
 ### ---        theoretically, stock prices aren't an ergodic process but their returns are, i.e. stock returns, 
 ### ---        which constitute the first-order differences of stock prices, are weakly stationary. 
 ### ---        For practical purposes though, standardizing also ensures that the performance all indices are 
